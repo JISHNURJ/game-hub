f263ed39e3c071c12902de52ab4ceb528dee02e2
Jishnu <jishnuraj551@gmail.com>
2024-07-15 23:27:48 +0400

    Sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,17 +1,56 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import React from "react";
 
-const SortSelector = () => {
+interface Props {
+  onSelectOrder: (order: string) => void;
+  sortOrderProp: string;
+}
+
+const SortSelector = ({ onSelectOrder, sortOrderProp }: Props) => {
+  const sortOrder = [
+    {
+      value: "",
+      label: "Relevance",
+    },
+    {
+      value: "-added",
+      label: "Date added",
+    },
+    {
+      value: "name",
+      label: "Name",
+    },
+    {
+      value: "-released",
+      label: "Released date",
+    },
+    {
+      value: "metacritic",
+      label: "Popularity",
+    },
+    {
+      value: "-rating",
+      label: "Average rating",
+    },
+  ];
+  const currentSortOrder = sortOrder.find(
+    (order) => order.value === sortOrderProp
+  );
   return (
     <Menu>
-      <MenuButton as={Button}>Order by : relevance</MenuButton>
+      <MenuButton as={Button}>
+        Order by {currentSortOrder?.label || "Relevance"}
+      </MenuButton>
       <MenuList>
-        <MenuItem>Relevance</MenuItem>
-        <MenuItem>date added</MenuItem>
-        <MenuItem>type</MenuItem>
-        <MenuItem>order</MenuItem>
-        <MenuItem>rating</MenuItem>
-        <MenuItem>descp</MenuItem>
+        {sortOrder.map((each) => (
+          <MenuItem
+            onClick={() => onSelectOrder(each.value)}
+            key={each.value}
+            value={each.value}
+          >
+            {each.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
